fba185767691fc8a927e226c8adf35796d5cd33b
Devansh Verma <Devansh616@gmail.com>
2018-01-13 00:01:44 +0530

    Add files via upload

---
+++ Fibonacci Series/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry Method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! It is a program which generates fibonacci series from 1 to 200");
+            GenerateFibonacciSeries();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Method which generates fibonacci series
+        /// </summary>
+        private static void GenerateFibonacciSeries()
+        {
+            int prev = 1,next = 1;
+            Console.WriteLine(prev);
+            while (next < 200)
+            {
+                next = prev + next;
+                prev = next - prev;
+                Console.WriteLine(prev);
+            }
+        }
+    }
+}
